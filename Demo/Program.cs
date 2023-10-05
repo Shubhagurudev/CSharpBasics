f8c59f3f17c6b692a9562e0ef2c66bf6d35efc24
shubh <shubh@DESKTOP-KLA8RPE>
2023-10-05 21:50:58 +0530

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WOrld!!!");
+            Console.WriteLine("Hello India!!!");
+            Console.WriteLine(777);
+            Console.WriteLine(7.77f);
+            int num = 100;
+            float count = 77.77f;
+            Console.WriteLine($"The value of num is {num}");
+            Console.WriteLine($"The value of count is {count}");
+            Console.Read();
+        }
+    }
+}
